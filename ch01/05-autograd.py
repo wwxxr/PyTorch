b0899a2cf6e597d1acf264057458e3213a40ce6c
wwxxr <116928513+wwxxr@users.noreply.github.com>
2023-10-23 15:48:39 +0800

    Update 05-autograd.py

--- ch01/05-autograd.py
+++ ch01/05-autograd.py
@@ -6,12 +6,12 @@ print('x:', x)
 print('x.grad:', x.grad)
 y = 2 * torch.dot(x, x)  # 2.记录目标值的计算
 print('y:', y)
-y.backward()  # 3.执行它的反向传播函数
+y.backward()  # 3.执行它的反向传播函数  通过调用反向传播函数来自动计算y关于x每个分量的梯度
 print('x.grad:', x.grad)  # 4.访问得到的梯度
 print('x.grad == 4*x:', x.grad == 4 * x)
 
 ## 计算另一个函数
-x.grad.zero_()
+x.grad.zero_()    # 在默认情况下，pytorch会累积梯度，我们需要清除之前的值
 y = x.sum()
 print('y:', y)
 y.backward()
@@ -24,7 +24,19 @@ y = x * x
 y.sum().backward()
 print('x.grad:', x.grad)
 
+# 将某些计算移动到记录的计算图之外
+x.grad.zero_()
+y = x * x
+u = y.detach()    # 把y当作一个常数，而不是关于x的函数，所有u对系统来讲，就是一个常数，此时y是x的函数，而u不是
+z = u * x
+z.sum().backward()
+x.grad == u
 
+x.grad.zero()
+y.sum().backward()
+x.grad == 2 * x
+
+# 即使构建函数的计算图需要通过python控制流（例如，条件、循环或任意函数调用），我们仍然可以计算得到的变量的梯度
 def f(a):
     b = a * 2
     print(b.norm())
@@ -36,6 +48,10 @@ def f(a):
         c = 100 * b
     return c
 
+a = torch.randn(size=(),requires_grad=True)
+d = f(a)
+d.backward()
+a.grad == d / a
 
 print('2.Python控制流的梯度计算')
 a = torch.tensor(2.0)  # 初始化变量
